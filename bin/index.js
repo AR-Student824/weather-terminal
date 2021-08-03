52ebfc3c075fd083db0b3523d07cde6e2475a79c
AR <techrebel2723@gmail.com>
2021-08-03 04:20:36 +0000

    done

--- bin/index.js
+++ bin/index.js
@@ -1,1 +1,31 @@
+#!/usr/bin/env node
 
+const chalk = require("chalk");
+const boxen = require("boxen");
+const axios = require("axios");
+console.info('[Loading] Please wait...')
+axios.get('https://weather.ar-dev.cf/api/json').then(res => {
+const celcius = res.data.c
+const farenheight = res.data.f 
+const location = res.data.location
+var types = []
+celcius.weather.forEach(x => {
+    types.push(x.main)
+})
+var sunrise = new Date(celcius.sys.sunrise * 1000).toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'})
+var sunset = new Date(celcius.sys.sunset * 1000).toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'})
+console.log(boxen(`${chalk.blue.bold(celcius.main.temp + '°C / ' + farenheight.main.temp + '°F')}`
+ + '\n' + chalk.red.bold(types.toString().replace(/,/, ' + ')) + chalk.black(' - ' + ' Feels like ' + celcius.main.feels_like + '°C / ' + farenheight.main.feels_like + '°F') 
+ + '\n' + chalk.red.bold(location.city) + chalk.black(', ' + location.region_code + ', ' + location.country_code)
+ + '\n' + chalk.red.bold('Humidity ' + celcius.main.humidity + '%') + chalk.black(', Wind speed ' + celcius.wind.speed)
+ + '\n' + chalk.red.bold('Sunrise ' + sunrise) + chalk.black(', Sunset ' + sunset)
+ , {
+    padding: 1,
+    margin: 1,
+    borderStyle: "round",
+    borderColor: "green",
+    backgroundColor: "white",
+    allign: "center"
+}))
+//console.log(res.data)
+})
